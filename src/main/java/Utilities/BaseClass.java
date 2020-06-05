1718dfbcc95a4d7056ebcb74e816f2e4b7fa25ec
unknown <snehaa91@gmail.com>
2020-06-05 16:57:57 +0530

    FirstTrial

---
+++ src/main/java/Utilities/BaseClass.java
@@ -0,0 +1,17 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	WebDriver driver = new ChromeDriver();
+	
+	@Test
+	public void test() {
+		
+	}
+	
+
+}
